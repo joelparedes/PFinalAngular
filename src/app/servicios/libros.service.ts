d808244a79b3838948ff6d6038f4caae97b5bd25
Joel Ezequiel Paredes Abreu <49132211+joelparedes@users.noreply.github.com>
2020-11-07 12:18:26 -0400

    Agregando metodos del service

--- src/app/servicios/libros.service.ts
+++ src/app/servicios/libros.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireList } from 'angularfire2/database';
+import { request } from 'http';
 import { Libro } from "../Modelos/libro";
 
 @Injectable({
@@ -15,18 +16,45 @@ export class LibrosService {
   }
 
   //Metodo para retonar todos los libros disponibles
-  getLibros(){}
+  getLibros(){
+    //Con esto asociamos la conexion remota a nuestra lista local
+    return this.libroList = this.angularFireDatabase.list('libro');
+  }
 
   //Recibira un id y retornara el libro correspondiente
-  getLibro(id: any){}
+  //getLibro(id: any){}
 
   //metodo para Agregar libro
-  addLibro(libro: Libro){}
+  addLibro(request: Libro){
+    this.libroList.push({
+      titulo: request.titulo,
+      sipnosis: request.sipnosis,
+      autor: request.autor,
+      cantPaginas: request.cantPaginas,
+      status: request.status,
+      fechaCreacion: request.fechaCreacion,
+      fechaPublicacion: request.fechaPublicacion,
+      imagePath: request.imagePath
+    })
+  }
 
   //metodo para actualizar libro
-  updateLibro(id: any, libro: Libro){}
+  updateLibro(id: any, request: Libro){
+    this.libroList.update(id, {
+      titulo: request.titulo,
+      sipnosis: request.sipnosis,
+      autor: request.autor,
+      cantPaginas: request.cantPaginas,
+      status: request.status,
+      fechaCreacion: request.fechaCreacion,
+      fechaPublicacion: request.fechaPublicacion,
+      imagePath: request.imagePath
+    })
+  }
 
   //metodo para eliminar libro
-  deleteLibro(id: any){}
+  deleteLibro(id: any){
+    this.libroList.remove(id);
+  }
 
 }
